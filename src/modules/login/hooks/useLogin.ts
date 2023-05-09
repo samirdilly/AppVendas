fd9b76b5b2545ff44342f69f9c4eb5f007b728b4
Samir Dilly <samir9.dilly@gmail.com>
2023-05-09 14:28:28 -0300

    feat create ConnectionAPI

--- src/modules/login/hooks/useLogin.ts
+++ src/modules/login/hooks/useLogin.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
-import { NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
+import { NativeSyntheticEvent, TextInputChangeEventData } from "react-native/types";
+import { connectionAPIPost } from "../../../shared/functions/connection/connectionAPI";
 
 export const useLogin = () => {
     const [email, setEmail] = useState<string>('');
@@ -11,7 +12,7 @@ export const useLogin = () => {
 
     const handleOnPress = async () => {
         setLoading(true);
-        const resultAxios = await axios.post('http://192.168.254.165:8080/auth', {
+       await connectionAPIPost('http://192.168.254.165:8080/auth', {
             email,
             password,
         }).catch(() => {
@@ -38,6 +39,6 @@ export const useLogin = () => {
         errorMessage,
         handleOnPress,
         handleOnChangeEmail,
-        handleOnChangePassword,
+        handleOnChangePassword
     }
 }
--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,18 +1,9 @@
-import {  NativeSyntheticEvent, TextInputChangeEventData, View } from "react-native"
+import { View } from "react-native"
 import { ContainerLogin, ImageLogo } from "../styles/login.styles";
 import Input from "../../../shared/components/input/Input";
 import Button from "../../../shared/components/button/Button";
-import Text from "../../../shared/components/text/Text";
 import { theme } from "../../../shared/themes/theme";
-import { Icon } from "../../../shared/components/icon/Icon";
-import axios from "axios";
-import { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
-
-
-
-
-
 
 
 const Login = () => {
@@ -31,8 +22,21 @@ const Login = () => {
         <View>
             <ContainerLogin>
                 <ImageLogo resizeMode="contain" source={require('../../../assets/images/logo.png')}/>
-                <Input value={email} onChange={handleOnChangeEmail}  errorMessage={errorMessage} margin="0px 0px 8px 0px" placeholder="Digite seu E-mail" title="E-mail:"/>
-                <Input value={password} onChange={handleOnChangePassword} errorMessage={errorMessage} secureTextEntry placeholder="Digite sua senha " title="Senha:"/>
+                <Input 
+                value={email} 
+                onChange={handleOnChangeEmail}  
+                errorMessage={errorMessage} 
+                margin="0px 0px 8px 0px" 
+                placeholder="Digite seu E-mail" 
+                title="E-mail:"
+                />
+                <Input 
+                value={password} 
+                onChange={handleOnChangePassword} 
+                errorMessage={errorMessage} 
+                secureTextEntry placeholder="Digite sua senha " 
+                title="Senha:"
+                />
                 <Button  
                 type={theme.buttons.buttonsTheme.primary} 
                 margin='16px' 
